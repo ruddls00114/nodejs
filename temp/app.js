1f9a9e7dc8e3d75d8657bdeb29ce7dc0065071c6
kyoungin <kkmm114@gmail.com>
2018-02-23 20:56:30 +0900

    정적파일 추가하는 방법

--- temp/app.js
+++ temp/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express()
+app.use(express.static('public'))
+//public이라는 디렉토리의 정적파일을 불러올수 있다.
+//localhost:3000/test.jpg , /test.txt 와 같이 /public/test.jpg 를 하지않아도 !
 app.get('/',function(req,res){
 	res.send("hello home page")
+})
+app.get('/route',function(req,res){
+	res.send('hello router, <img src ="/file.jpg">')
 })
 app.get('/login',function(req,res){
 	res.send("<h2>login please<h2>")

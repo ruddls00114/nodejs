accc3a75e198a3351fb9db3551b210d515d2351f
kyoungin <kkmm114@gmail.com>
2018-03-01 16:42:54 +0900

    의미론적인 url

--- temp/app.js
+++ temp/app.js
@@ -1,37 +1,29 @@
 const express = require('express');
 const app = express()
-app.use(express.static('public'))
-//public이라는 디렉토리의 정적파일을 불러올수 있다.
-//localhost:3000/test.jpg , /test.txt 와 같이 /public/test.jpg 를 하지않아도 !
-//서버를 가동시킨 후에 정적인 파일을 변경시키고 리로드 하면 서버를 재가동하지않아도 반영됨
+
+	/*
+	expressjs.com/
+	의미론적인 URL 
+	*/
+app.get('/topic',function(req,res){
+	var topics =[
+		'javascript is',
+		'nodejs is ...',
+		'express is ...'
+	]
+	var output= `
+	<a href = "/topic?id=0">javascript</a><br>
+	<a href = "/topic?id=1">nodejs</a><br>
+	<a href = "/topic?id=2">express</a><br><br>
+	${topics[req.query.id]}
+	`
+	res.send(output); 	//요청된 쿼리스트링id의 값
+
+})
 app.get('/',function(req,res){
 	res.send("hello home page")
 })
-app.get('/route',function(req,res){
-	res.send('hello router, <img src ="/file.jpg">')
-})
-app.get('/dynamic',function(req,res){ 
-	//서버를 가동시킨 후에 동적파일을 변경시킨다면 서버를 재가동 시켜야함 .
-	var time = Date()
-	var lis =''
-	for(var i =0; i<5;i++){
-		lis = lis + 'coding'
-	}
-	var output = `<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>Document</title>
-	</head>
-	<body>
-		Hello dynamic
-		${time}
-		${lis}
-	</body>
-	</html>
-	`
-	res.send(output)
-})
+
 app.get('/login',function(req,res){
 	res.send("<h2>login please<h2>")
 })
